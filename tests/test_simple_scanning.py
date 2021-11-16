ed29cd077cebec98a0f683cd20632b71f15be050
Eric Carmichael <eric@ckcollab.com>
2021-11-15 20:52:04 -0800

    generalize test class, add etherael check, remove segmentation test for now since we may leverage botty segmentation

--- tests/test_simple_scanning.py
+++ tests/test_simple_scanning.py
@@ -1,36 +1,19 @@
-import numpy as np
-
-from unittest import TestCase
-from PIL import Image
-
+from .base import OCRTestCases
 from ..ocr import OCR
 
 
-class SimpleScanningTestcases(TestCase):
+class SimpleScanningTestcases(OCRTestCases):
 
     def setUp(self):
         self.ocr = OCR()
 
-    def _img_to_np(self, path):
-        img = Image.open(path)
-        return np.asarray(img, dtype='uint8')
-
     def test_scanning_simple_works(self):
-        readings = self.ocr.read(self._img_to_np("tests/test_data/simple/586_gold.png"))
-        assert len(readings) == 1
-        _, text, item_type = readings[0]
-        assert text == "586 Gold"
-        assert item_type == "Normal"
-
-        readings = self.ocr.read(self._img_to_np("tests/test_data/simple/super_mana_potion.png"))
-        assert len(readings) == 1
-        _, text, item_type = readings[0]
-        assert text == "Super Mana Potion"
-        assert item_type == "Normal"
+        self._check_scan("tests/test_data/simple/586_gold.png", "586 Gold", "Normal")
+        self._check_scan("tests/test_data/simple/super_mana_potion.png", "Super Mana Potion", "Normal")
 
     def test_scanning_ethereal_works(self):
-        assert False
+        self._check_scan("tests/test_data/ethereal/fascia.png", "Fascia", "Socketed/Ethereal")
 
-    def test_segmentation_not_misgrouping_things(self):
-        # Get a few images of lots of items near each other and confirm grouping is proper
-        assert False
+    # def test_segmentation_not_misgrouping_things(self):
+    #     # Get a few images of lots of items near each other and confirm grouping is proper
+    #     assert False
